f274dc4a8a9295965277efb89077ada2310f5a6a
Lehlogonolo N. Masubelele <codesenju@gmail.com>
2020-06-16 21:37:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000
 
 //Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('Hello World');
+ res.send('Dumelang');
 });
 
 //listen to port 3000 by default
